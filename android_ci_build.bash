93472250538baeb074e065a047a115b94246c72a
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2024-11-10 15:14:24 +0800

    Update android_ci_build.bash

--- android_ci_build.bash
+++ android_ci_build.bash
@@ -8,7 +8,7 @@ cmake_build () {
   mkdir -p build-$ANDROID_ABI
   mkdir -p lib/$ANDROID_ABI
   cd build-$ANDROID_ABI
-  cmake $GITHUB_WORKSPACE -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DANDROID_STL=system -DANDROID_LD=lld -DANDROID_PLATFORM=24 -DANDROID_ABI=$ANDROID_ABI -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_LATEST_HOME/build/cmake/android.toolchain.cmake
+  cmake $GITHUB_WORKSPACE -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DANDROID_STL=c++_shared -DANDROID_LD=lld -DANDROID_PLATFORM=24 -DANDROID_ABI=$ANDROID_ABI -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_LATEST_HOME/build/cmake/android.toolchain.cmake
   cmake --build . --clean-first
   # | echo "Build exit code: $?"
   # --verbose
