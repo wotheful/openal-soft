269b09585a1fe3bda86790e89a66c104899feb40
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-05-02 13:31:02 +0800

    更新 android_ci_build.bash

--- android_ci_build.bash
+++ android_ci_build.bash
@@ -8,14 +8,11 @@ cmake_build () {
   mkdir -p build-$ANDROID_ABI
   mkdir -p lib/$ANDROID_ABI
   cd build-$ANDROID_ABI
-  cmake $GITHUB_WORKSPACE -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DANDROID_LD=lld -DANDROID_PLATFORM=24 -DANDROID_ABI=$ANDROID_ABI -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_LATEST_HOME/build/cmake/android.toolchain.cmake
-  cmake --build . --clean-first
-  # | echo "Build exit code: $?"
-  # --verbose
+  cmake $GITHUB_WORKSPACE -DANDROID_PLATFORM=24 -DANDROID_ABI=$ANDROID_ABI -DCMAKE_SYSTEM_NAME=Android -DANDROID_TOOLCHAIN=clang -DANDROID_ARM_MODE=arm -DCMAKE_MAKE_PROGRAM=$ANDROID_NDK_LATEST_HOME/prebuilt/linux-x86_64/bin/make -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_LATEST_HOME/build/cmake/android.toolchain.cmake
+  cmake --build . --config Release --parallel 4
   cd ..
+  $ANDROID_NDK_LATEST_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-strip $GITHUB_WORKSPACE/build-$ANDROID_ABI/libopenal.so
   cp build-$ANDROID_ABI/libopenal.so lib/$ANDROID_ABI/
 }
-
-mkdir -p lib
 
 cmake_build arm64-v8a
