0bfd45436726dd2d989cc9a73ed3c6dea2629992
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-05-02 12:32:29 +0800

    Create version.h

---
+++ include/AL/version.h
@@ -0,0 +1,14 @@
+#ifndef AL_VERSION_H
+#define AL_VERSION_H
+
+/* Define to the library version */
+#define ALSOFT_VERSION "1.24.3"
+#define ALSOFT_VERSION_NUM 1,24,3,0
+
+/* Define the branch being built */
+#define ALSOFT_GIT_BRANCH ""
+
+/* Define the hash of the head commit */
+#define ALSOFT_GIT_COMMIT_HASH ""
+
+#endif
