2fe44d1bcf1e6e1625a77c6da51a54cdb9058063
wotheful <hi114514@petalmail.com>
2025-06-14 15:59:57 +0800

    Update define.cpp

--- define.cpp
+++ define.cpp
@@ -5,8 +5,8 @@
 #include "alc/context.h"
 #include "alstring.h"
 
-ALboolean AL_APIENTRY alIsExtensionPresentDirect(ALCcontext *context, const ALchar *extName);
-ALvoid* AL_APIENTRY alGetProcAddressDirect(ALCcontext*, const ALchar *funcName);
-ALenum AL_APIENTRY alGetEnumValueDirect(ALCcontext*, const ALchar *enumName);
-AL_API ALvoid* AL_APIENTRY alGetProcAddress(const ALchar *funcName);
-AL_API ALenum AL_APIENTRY alGetEnumValue(const ALchar *enumName);
+ALboolean AL_APIENTRY alIsExtensionPresentDirect(ALCcontext *context, const ALchar *extName) AL_API_NOEXCEPT;
+ALvoid* AL_APIENTRY alGetProcAddressDirect(ALCcontext*, const ALchar *funcName) AL_API_NOEXCEPT;
+ALenum AL_APIENTRY alGetEnumValueDirect(ALCcontext*, const ALchar *enumName) AL_API_NOEXCEPT;
+AL_API ALvoid* AL_APIENTRY alGetProcAddress(const ALchar *funcName) AL_API_NOEXCEPT;
+AL_API ALenum AL_APIENTRY alGetEnumValue(const ALchar *enumName) AL_API_NOEXCEPT;
