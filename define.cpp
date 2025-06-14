36c88c695d1ffe610e875b6ce3d7e0cc3f183713
wotheful <hi114514@petalmail.com>
2025-06-14 15:43:36 +0800

    Create define.cpp

---
+++ define.cpp
@@ -0,0 +1,13 @@
+#include "config.h"
+#include "AL/al.h"
+#include "AL/alc.h"
+
+#include "alc/context.h"
+#include "alstring.h"
+#include "direct_defs.h"
+
+ALboolean AL_APIENTRY alIsExtensionPresentDirect(ALCcontext *context, const ALchar *extName);
+ALvoid* AL_APIENTRY alGetProcAddressDirect(ALCcontext*, const ALchar *funcName);
+ALenum AL_APIENTRY alGetEnumValueDirect(ALCcontext*, const ALchar *enumName);
+AL_API ALvoid* AL_APIENTRY alGetProcAddress(const ALchar *funcName);
+AL_API ALenum AL_APIENTRY alGetEnumValue(const ALchar *enumName);
